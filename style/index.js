4e4d56b7fbee15c74d038e2ec7f0bb86fd2eef4a
Schonej <43455575+Schonej@users.noreply.github.com>
2021-03-19 03:44:08 -0600

    commit

--- style/index.js
+++ style/index.js
@@ -13,15 +13,15 @@ var swiper = new Swiper('#swiper1', {
       slidesPerView: 1,
       spaceBetween: 10,
     },
-    '@0.75': {
+    '@0.50': {
       slidesPerView: 2,
       spaceBetween: 20,
     },
-    '@1.00': {
+    '@0.75': {
       slidesPerView: 3,
       spaceBetween: 40,
     },
-    '@1.50': {
+    '@1.00': {
       slidesPerView: 4,
       spaceBetween: 50,
     },
